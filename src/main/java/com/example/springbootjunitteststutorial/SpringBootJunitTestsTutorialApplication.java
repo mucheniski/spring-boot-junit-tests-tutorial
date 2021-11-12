8328d453c486659787e0c20e2c1932ee84014103
Diego Mucheniski <mucheniski@gmail.com>
2021-11-12 13:37:44 -0300

    projeto criado

---
+++ src/main/java/com/example/springbootjunitteststutorial/SpringBootJunitTestsTutorialApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootjunitteststutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJunitTestsTutorialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootJunitTestsTutorialApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springbootjunitteststutorial/SpringBootJunitTestsTutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootjunitteststutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootJunitTestsTutorialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
